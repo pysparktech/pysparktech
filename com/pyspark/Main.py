ab26702b444b8889042a5d4a87231f674dbf689a
viewp <prasadaws77@gmail.com>
2023-08-27 20:12:06 +0530

    csv file read activitites

--- com/pyspark/Main.py
+++ com/pyspark/Main.py
@@ -1,1 +1,25 @@
-print("Pyspark tech")
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("csv reading").master("local").getOrCreate()
+
+filepath = "C:\\Users\\viewp\\OneDrive\\Desktop\\data\\accidents_2017.csv"
+
+#create table tablename(column datatype.......)
+# spark : _c0........... (string)
+# storage :" file formats -> parquet , orc, rc, csv, text, json, xml, avro .... etc.,
+readOptions = {
+    "header":"true",
+    "inferSchema":"true"
+}
+#df = spark.read.option("header","true").option("inferSchema","true").csv(filepath)
+df = spark.read.csv(filepath,**readOptions)
+
+#desc tablename
+df.printSchema()
+
+# select * from table limit 5
+df.show(5,truncate=0)
+
+targetPath = "C:\\Users\\viewp\\OneDrive\\Desktop\\data\\output"
+# insert into targetable (.......)
+df.write.mode("overwrite").parquet(targetPath)
